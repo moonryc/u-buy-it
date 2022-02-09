caecc8dea635329d3ee8a148e1396967f7a42c3e
moonryc <70920813+moonryc@users.noreply.github.com>
2022-02-09 15:10:05 -0600

    updated models line indents and updated product to have the stock default value be 10 and validate it as numeric rather than as an integer

--- models/Tag.js
+++ models/Tag.js
@@ -1,29 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const {Model, DataTypes} = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model {
+}
 
 Tag.init(
-  {
-    // define columns
-    id:{
-      type:DataTypes.INTEGER,
-      allowNull:false,
-      autoIncrement:true,
-      primaryKey:true
+    {
+        // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        tag_name: {
+            type: DataTypes.STRING
+        }
     },
-    tag_name:{
-      type:DataTypes.STRING
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'tag',
     }
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'tag',
-  }
 );
 
 module.exports = Tag;
